1183b768b1372276afda5acf625f937a33bbf076
rahulyadav753 <ry1951947@gmail.com>
2026-08-10 16:02:12 +0530

    Task 3 completed

--- Day-1/QuotesApi/Program.cs
+++ Day-1/QuotesApi/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using QuotesApi.Data;
+using QuotesApi.Extensions;
+using QuotesApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Database
+builder.Services.AddInfrastructure(builder.Configuration);
+
+// Repository
+builder.Services.AddScoped<IQuoteRepository, QuoteRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// API endpoints
+app.MapQuoteEndpoints();
+
+// Create/update database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<QuotesDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
--- Day-1/QuotesApi/obj/Debug/net10.0/QuotesApi.AssemblyInfo.cs
+++ Day-1/QuotesApi/obj/Debug/net10.0/QuotesApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd047fa84e14138cc06e59d044a6474084366962")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b23abde2a4e4431f0c7deea0dbe87e130da84a0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
