0232ae88ab20b3899357a0231d55cdb2d92c9d67
rahulyadav753 <ry1951947@gmail.com>
2026-08-12 16:50:40 +0530

    Task-2 Complete authentication testing and CI

--- Day-3/QuotesApi/obj/Debug/net10.0/QuotesApi.AssemblyInfo.cs
+++ Day-3/QuotesApi/obj/Debug/net10.0/QuotesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a56e825ec91aeffc46149557cde9d246ae9b6b23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b0531ca9229e3bc9f3b161653357ec22624ea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
