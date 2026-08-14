247c88a993425949d9609366d3a63464d7c7c7b2
rahulyadav753 <ry1951947@gmail.com>
2026-08-14 15:37:04 +0530

    DAY-5 TASK 4 Deploy Quotes API with Azure Developer CLI

--- Day-3/QuotesApi/obj/Debug/net10.0/QuotesApi.AssemblyInfo.cs
+++ Day-3/QuotesApi/obj/Debug/net10.0/QuotesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d2eae1674c6f1eb96a18e0019e3df1e5fd3b41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4dd032ba7439efa5040afb71987aeb121888681")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuotesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
